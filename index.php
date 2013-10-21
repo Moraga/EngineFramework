e36b7be7d8e330e6429ed5fa7f3a3a46c2a7fdf8
Alejandro Moraga <moraga86@gmail.com>
2013-10-21 18:37:51 -0200

    Fixed the URL constant value when installed at the root directory

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 define('DIR', dirname(__FILE__) .'/');
-define('URL', dirname($_SERVER['PHP_SELF']) .'/');
+define('URL', substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/') + 1));
 
 require DIR .'settings.php';
 require LIB .'DB.php';
